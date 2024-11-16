fc022c3b30b1ea114fad8ab2289775e1197d0755
abudaan <daniel@abumarkub.net>
2024-11-16 22:25:50 +0100

    render a table with data

--- src/component/text.rs
+++ src/component/text.rs
@@ -1,6 +1,6 @@
 use leptos::*;
 
-use crate::model::text::get_all_texts;
+use crate::model::{text::get_all_texts, Text};
 
 #[component]
 pub fn TextTable() -> impl IntoView {
@@ -10,6 +10,8 @@ pub fn TextTable() -> impl IntoView {
       <Suspense
         fallback=move || view! { <p>"Loading texts..."</p> }
       >
+        <h2>"Data"</h2>
+
         <div className="d-flex justify-content-between align-items-center">
           <h1>"Texts"</h1>
           <a className="btn btn-primary btn"  href="/somewhere">
@@ -30,27 +32,73 @@ pub fn TextTable() -> impl IntoView {
             </tr>
           </thead>
           <tbody>
-            <For
-              each=move || texts.get().unwrap().unwrap()
-              key=|text|text.id
-              children=move |text| {
-                view! {
-                  <tr>
-                    <td>{text.title}</td>
-                    <td>{text.published}</td>
-                    <td align="right">
-                      <a class="btn btn-outline-primary"
-                        href="admin_text_edit">
-                        <i class="bi bi-pencil me-1"></i>
-                        "Edit"
-                      </a>
-                    </td>
-                  </tr>
-                }
-              }
-            />
+
+
+        {
+          move || match texts.get() {
+              None => view! { <p>"Loading..."</p> }.into_view(),
+              Some(t) => t.unwrap().into_iter().map(|text| {
+                      view! {
+                        <tr>
+                          <td>{text.title}</td>
+                          <td>{text.published}</td>
+                          <td align="right">
+                            <a class="btn btn-outline-primary"
+                              href="admin_text_edit">
+                              <i class="bi bi-pencil me-1"></i>
+                              "Edit"
+                            </a>
+                          </td>
+                        </tr>
+                      }
+                   }).collect_view()
+            }
+          }
           </tbody>
         </table>
-      </Suspense>
+        </Suspense>
     }
 }
+// <div className="d-flex justify-content-between align-items-center">
+//   <h1>"Texts"</h1>
+//   <a className="btn btn-primary btn"  href="/somewhere">
+//     <i className="bi bi-plus"></i>
+//     "New text"
+//   </a>
+// </div>
+// <table className="table table-hover">
+//   <thead>
+//     <tr>
+//       <th scope="col" class="">
+//         <a>"Title"</a>
+//       </th>
+//       <th scope="col" class="">
+//         <a>"Published"</a>
+//       </th>
+//       <th/>
+//     </tr>
+//   </thead>
+//   <tbody>
+
+//   <For
+//     each=move || texts.get().unwrap().unwrap()
+//     key=|text|text.id
+//     children=move |text| {
+//       view! {
+//         <tr>
+//           <td>{text.title}</td>
+//           <td>{text.published}</td>
+//           <td align="right">
+//             <a class="btn btn-outline-primary"
+//               href="admin_text_edit">
+//               <i class="bi bi-pencil me-1"></i>
+//               "Edit"
+//             </a>
+//           </td>
+//         </tr>
+//       }
+//     }
+//   />
+
+//   </tbody>
+//   </table>
