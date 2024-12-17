788c3fce9f12f010ffb9ced1c3c4b872671730f7
abudaan <daniel@abumarkub.net>
2024-12-17 16:01:28 +0100

    wip error signal

--- src/app.rs
+++ src/app.rs
@@ -149,34 +149,54 @@ fn Test1() -> impl IntoView {
         }
     });
 
-    let (error, set_error) = signal("".to_string());
+    let (error, set_error) = signal(true);
+    let mut index = 0;
+    let print_error = move || {
+        let a = action_value.get();
+        let b = error.get();
+        if b {
+            a
+        } else {
+            index += 1;
+            format!("xx {}", index)
+        }
+    };
 
-    // let derived_signal_double_count = move || value.get();
-    // derived_signal_double_count.set("aap".to_string());
+    let on_submit = move |ev: ev::SubmitEvent| {
+        // tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        set_error(true)
+    };
 
     // Effect::new_isomorphic(move |_| {
-    //     logging::log!("Got value = {:?}", value.get());
+    //     logging::log!("Got value = {:?}", action_value.get());
     // });
 
-    Effect::watch(
-        move || action_value.get(),
-        move |val, _prev_val, _| {
-            set_error.set(val.to_owned());
-        },
-        false,
-    );
+    // Effect::watch(
+    //     move || action_value.get(),
+    //     move |val, _prev_val, _| {
+    //         set_error.set(val.to_owned());
+    //     },
+    //     false,
+    // );
 
     view! {
         // <ErrorBoundary
         //     fallback=move |error| {
         //         move || format!("{:#?}", error.get())
         //     }>
-            <pre>{error}</pre>
-            <ActionForm action=test_action>
+            <pre>{print_error}</pre>
+            <ActionForm
+                action=test_action
+                // on:submit=on_submit
+            >
                 <input type="text" name="value"
-                    on:focus=move|_ev|set_error.set(String::new())
+                    on:focus=move|_ev|set_error.set(false)
+                    on:blur=move|_ev|set_error.set(true)
                 />
-                <button type="submit">"Submit"</button>
+                <button type="submit" on:hover=move|_:leptos::ev::Event|{
+                    logging::log!("hover!");
+                    set_error.set(true);
+                }>"Submit"</button>
             </ActionForm>
         // </ErrorBoundary>
     }
